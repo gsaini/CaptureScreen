d7137ee4d815bdba8edde87dc407d58ecc5bfb21
gsaini <gopal.saini.work@gmail.com>
2014-02-01 19:43:32 +0530

    Replace data values on client side.

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+	app = express(),
+	fs = require("fs");
+	
 app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 
-var fs = require("fs");
 app.post('/saveScreen', function(req, res){	
-	var fileName = __dirname + '/uploads/' + req.body.fileName + '.jpg',
-		base64Data = req.body.imgBase64.replace(/^data:image\/png;base64,/,"");
-
-	fs.writeFile(fileName, new Buffer(base64Data, "base64"), function(err) {
+	var fileName = __dirname + '/uploads/' + req.body.fileName + '.jpg';
+	fs.writeFile(fileName, new Buffer(req.body.imgBase64, "base64"), function(err) {
 		err ? res.send('Oops! something bad happened. :(')  : res.send("Woow! screen saved successfully. :)");		
 	});
 });
